ec5ab4b9e24cf92338378e8078d41f9481a78034
Pradeep Mehta (NCS) <pradeep.mehta@in.ncs-i.com>
2022-09-28 12:16:07 +0530

    first commit

---
+++ Controller/UserController.cs
@@ -0,0 +1,61 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JwtApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Admins")]
+        [Authorize(Roles ="Administrator")]
+        public IActionResult AdminsEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi, {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+        [HttpGet("Sellers")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellersEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi, {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("Public")]
+        public IActionResult Public()
+        {
+            return Ok("Hi, you're on public property");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if(identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    UserName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value,
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/UserConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel(){UserName="jason_admin",EmailAddress="jason.admin@email.com", Password="Pass_word",
+                    GivenName="Jason", Surname="Bryant", Role="Administrator"  },
+
+            new UserModel(){UserName="elyse_seller",EmailAddress="elyse.seller@email.com", Password="Pass_word",
+                    GivenName="Elyse", Surname="Lambert", Role="Seller"  },
+
+        };
+    }
+}
